5a6c3bc29f9788e135569f5a1faa4d569577b454
Jotiram-2004 <jotiramkumbhar435@gmail.com>
2024-08-09 08:59:58 +0530

    HII

--- Login/login.php
+++ Login/login.php
@@ -5,9 +5,6 @@
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Login</title>
       <link rel="stylesheet" href="login.css">
-      <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
    </head>
    <script>
       function switchAdminLogin(){
@@ -24,51 +21,48 @@
           document.getElementById("admin-image").style.display="none";
       }
    </script>
-   <body>
-      <div class="container">
+   <body style="margin:0;padding:0">
+      <div style="display:flex">
          <!-- left  -->
-         <div id="left-container" class="left-container">
-            <div id="emp-image" class="emp-image">
+         <div id="left-container" style="width:50%;">
+            <div id="emp-image" class="emp-image" style="width:100%;height:100%" >
                <img  src="../assets/emp.jpg" style="width:100%;height:100%" />
             </div>
-            <div id="admin-image" style="width:100%;height:100%">
-               <img  src="../assets/admin-1.jpg" style="width:100%;height:100%"/>
+            <div id="admin-image" style="width:100%;height:100%" >
+               <img  src="../assets/admin.jpg" style="width:100%;height:100%" />
             </div>
          </div>
          <!-- right -->
-         <div id="right-container">
+         <div id="right-container" style="width:50%;flex-direction:column">
             <!-- EMPLOYEE FORM -->
-            <div id="emp-form">
-               <div class="emp-form-container">
-                  <p>EMPLOYEE LOGIN</p>
+            <div id="emp-form" style="width:100%;height:100vh">
+               <div  style="display:flex;flex-direction:column;height:100%;">
+                  <h2>EMPLOYEE LOGIN</h2>
                   <form action="#" method="post">
                      <h6 for="employee-id">Employee Unique ID</h6>
                      <input type="text" id="employee-id" name="employee-id" placeholder="Enter your unique ID" required>
                      <h6 for="password">Password</h6>
                      <input type="password" id="password" name="password" placeholder="Enter your password" required>
-                     <button type="submit">LOGIN</button><button onclick="switchAdminLogin()" id="adminlogin-btn" type="submit">ADMIN LOGIN</button>
-                     </div>
-                   
+                     <button type="submit">LOGIN</button>
                   </form>
-                  <!-- <button onclick="switchAdminLogin()" id="adminlogin-btn" type="submit">ADMIN LOGIN</button> -->
+                  <button onclick="switchAdminLogin()" id="adminlogin-btn" type="submit">ADMIN LOGIN</button>
                </div>
             </div>
             <!-- ADMIN FORM  -->
-            <div id="admin-form">
-               < class="admin-form-container">
-                  <p>ADMIN LOGIN</p>
+            <div id="admin-form" style="width:100%;height:100vh">
+               <div  style="display:flex;flex-direction:column;height:100%;">
+                  <h2>ADMIN LOGIN</h2>
                   <form action="#" method="post">
-                     <h6 for="employee-id">Email ID/ Mobile No.</h6>
-                     <input type="email/No" id="employee-id" name="employee-id" placeholder="Enter Email/no" required>
+                     <h6 for="employee-id">Employee Unique ID</h6>
+                     <input type="text" id="employee-id" name="employee-id" placeholder="Enter your unique ID" required>
                      <h6 for="password">Password</h6>
                      <input type="password" id="password" name="password" placeholder="Enter your password" required>
                      <button  type="submit">LOGIN</button>
                   </form>
                   <button onclick="switchEmpLogin()" id="emplogin-btn" type="submit">EMPLOYEE LOGIN</button>
-                  <h3>New Register</h3>
-             </div>
-   </div>
-             <!--  -->
+               </div>
+            </div>
+            <!--  -->
          </div>
       </div>
 </html>
